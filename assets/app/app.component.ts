a9b0255e984870d57e232e0fc4c6cbe7f1b6fa2b
Matt Lunalover <mlunalover@kabbage.com>
2017-02-02 15:08:48 -0500

    moved message list into own component and created message input

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import {Message} from "./messages/message.model";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Matt'),
-        new Message('Some message', 'Matt')
-    ];
+
 }
