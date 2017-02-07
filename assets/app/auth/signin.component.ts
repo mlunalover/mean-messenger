1007c63426918901dbd599bd93a893adc77cda56
Matt Lunalover <mlunalover@kabbage.com>
2017-02-07 10:58:02 -0500

    sign in and signup component forms

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,8 +1,24 @@
 import {Component} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
 export class SigninComponent    {
+    myForm: FormGroup;
 
+    onSubmit()  {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+
+    ngOnInit()  {
+        this.myForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,8 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html'
 })
-export class SignupComponent    {
+export class SignupComponent implements OnInit{
+    myForm: FormGroup;
 
+    onSubmit()  {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+
+    ngOnInit()  {
+        this.myForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
