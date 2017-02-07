972035325f7b91318ccb118094a6c77e5a964ba5
Matt Lunalover <mlunalover@kabbage.com>
2017-02-07 16:04:32 -0500

    wired delte up to mongodb

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -30,7 +30,10 @@ export class MessageComponent   {
     }
 
     onDelete()    {
-        this.messageService.deleteMessage(this.message);
+        this.messageService.deleteMessage(this.message)
+            .subscribe(
+                result => console.log(result)
+            );
     }
 
 }
