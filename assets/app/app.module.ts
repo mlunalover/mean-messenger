999dadc1190375ce67579cbaf258b8a2c6fd7a11
Matt Lunalover <mlunalover@kabbage.com>
2017-02-08 09:38:08 -0500

    adjusting front end with edit button

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -14,6 +14,7 @@ import {LogoutComponent} from "./auth/logout.component";
 import {SignupComponent} from "./auth/signup.component";
 import {SigninComponent} from "./auth/signin.component";
 import {HttpModule} from "@angular/http";
+import {AuthService} from "./auth/auth.service";
 
 @NgModule({
     declarations: [
@@ -34,6 +35,7 @@ import {HttpModule} from "@angular/http";
         ReactiveFormsModule,
         HttpModule
     ],
+    providers: [AuthService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
